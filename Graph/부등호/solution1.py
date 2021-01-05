5d3af67999aa292a759cc533dd50f69b9c806a1a
Jungin <jungin3486@gmail.com>
2021-01-05 22:02:36 +0900

    solved

--- Graph/부등호/solution1.py
+++ Graph/부등호/solution1.py
@@ -1,41 +1,33 @@
-# n = int(input()) #부등호 개수
-# #부등호
-# sign = input().split()
-# #숫자 개수 = 부등호개수+1
-# max_list = list([i for i in range(0,n+1)])
-# min_list = list([i for i in range(9,9-(n+1),-1)])
-
-# print(max_list)
-# print(min_list)
-# print(sign)
-
-#https://rebas.kr/755
-n = int(input())
-sign = input().split()
-c = [False]*10
+n = int(input()) # 부등호 개수
+sign = input().split() #부등호
+check = [False]*10 # 0~9
 mx, mn = "", ""
 
-def possible(i, j, k):
+# 부등호가 성립하는 지 확인하는 함수
+def possible(i,j,k):
     if k == '<':
-        return i < j
+        return i<j
     if k == '>':
-        return i > j
+        return i>j
     return True
 
-def solve(cnt, s):
+def solve(cnt, z):
     global mx, mn
     if cnt == n+1:
-        if not len(mn):
-            mn = s
-        else:
-            mx = s
+        if not len(mn): # mn이 비워져 있으면 최솟값에 z 넣기
+            mn = z
+        else: # mn에 값이 있으면 최댓값에 넣기
+            mx = z
         return
-    for i in range(10):
-        if not c[i]:
-            if cnt == 0 or possible(s[-1], str(i), sign[cnt-1]):
-                c[i] = True
-                solve(cnt+1, s+str(i))
-                c[i] = False
+    for i in range(10):# 0부터 9까지
+        if not check[i]: # check[i]이 false이면(중복 방지)
+            if cnt==0 or possible(z[-1], str(i), sign[cnt-1]):
+                check[i]=True
+                solve(cnt+1, z+str(i)) #재귀함수
+                # 만약 0 > ? 일 경우 ?에 올 숫자가 없으므로 solve함수가 끝나고 check[0]이 false가 되고 check[1]부터 다시 시작한다.
+                check[i]=False
 
-solve(0, "")
-print("%s\n%s" % (mx, mn))
+
+solve(0, "") #solve 함수 돌리기
+print(mx)
+print(mn)
