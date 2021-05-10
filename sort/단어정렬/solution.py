d684ed2f89ff3e903812fb991c3e9392abdf4be9
Jungin <jungin3486@gmail.com>
2021-05-10 22:28:24 +0900

    단어정렬 코드 수정

--- sort/단어정렬/solution.py
+++ sort/단어정렬/solution.py
@@ -1,13 +1,8 @@
-12
-Junkyu 50 60 100
-Sangkeun 80 60 50
-Sunyoung 80 70 100
-Soong 50 60 90
-Haebin 50 60 100
-Kangsoo 60 80 100
-Donghyuk 80 60 100
-Sei 70 70 70
-Wonseob 70 70 90
-Sanghyun 70 70 80
-nsj 80 80 80
-Taewhan 50 60 90
+n = int(input())
+words = [input() for _ in range(n)]
+words = list(set(words))
+words.sort() #알파벳 순
+words.sort(key=len) #길이순
+
+for word in words:
+    print(word)
