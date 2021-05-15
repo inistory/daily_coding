a4024d8e30728580cd0931d0f6e6506e3aaf2741
Jungin <jungin3486@gmail.com>
2021-05-15 23:21:27 +0900

    단어뒤집기

---
+++ Stack&Queue/단어뒤집기/solution.py
@@ -0,0 +1,18 @@
+# import sys
+
+# N = int(sys.stdin.readline()) 
+# # score = [list(sys.stdin.readline().split()) for _ in range(N)]
+# word = list()
+
+# for i in range(N):
+#     word = list(sys.stdin.readline().split())
+#     print(word)
+#     # reversed_str = []
+#     # result = ''
+
+#     # for w in word:
+#     #     for k in range(len(w)):
+#     #         reversed_str += list(w[k]).pop()
+#     #     print(reversed_str)
+#     #         # reversed_word = ''.join(reversed(word[j]))
+#     #         # print(reversed_word)
