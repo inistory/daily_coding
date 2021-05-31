72d058f88350237773cdaabc4d2eab71d9a744fd
Jungin <jungin3486@gmail.com>
2021-05-31 22:26:25 +0900

    210531 멀쩡한 사각형

--- 프로그래머스/격자/멀쩡한사각형/solution.py
+++ 프로그래머스/격자/멀쩡한사각형/solution.py
@@ -1,12 +1,5 @@
 def solution(w,h):
-    if h < w:
-        if w%2==0:
-             answer = w*h - h *(w/2)
-        else:
-            answer = w*h -  h *((w-1)/2) + h
+    if w < h:
+        return w * h//2 *2
     else:
-        if h%2==0:
-             answer =  w*h -w *(h/2)
-        else:
-            answer =  w*h -w *((h-1)/2) + w
-    return answer
+        return h * w//2 *2
