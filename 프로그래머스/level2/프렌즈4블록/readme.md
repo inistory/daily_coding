ffbd64db7b63cafd7f4d9ec3aefb5522fcfab411
Jungin <jungin3486@gmail.com>
2021-06-09 23:24:12 +0900

    add 프렌즈블록4

---
+++ 프로그래머스/level2/프렌즈4블록/readme.md
@@ -0,0 +1,10 @@
+## 1. 문제 설명
+
+[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/17679)
+
+- 입력
+  m : 높이
+  n : 폭
+  board: 배치정보
+- 출력
+  지워진 블록 수
--- 프로그래머스/level2/프렌즈4블록/solution.py
+++ 프로그래머스/level2/프렌즈4블록/solution.py
@@ -1,7 +1,7 @@
 def pop_num(b, m, n):
     pop_set = set()
-    for i in range(1, n):
-        for j in range(1, m):
+    for i in range(1, m):
+        for j in range(1, n):
             if b[i][j] == b[i-1][j-1] == b[i-1][j] == b[i][j-1] != '_':
                 pop_set |= set([(i, j), (i-1, j-1), (i-1, j), (i, j-1)])
 
