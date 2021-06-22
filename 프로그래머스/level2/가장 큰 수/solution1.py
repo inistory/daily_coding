14c256fb1099245e71280c56b4f11ff157979a4e
Jungin <jungin3486@gmail.com>
2021-06-22 23:04:55 +0900

    update solution1.py of 가장 큰 수

--- 프로그래머스/level2/가장 큰 수/solution1.py
+++ 프로그래머스/level2/가장 큰 수/solution1.py
@@ -1,4 +1,9 @@
 def solution(numbers):
-    numbers = list(map(str, numbers)) 
-    numbers.sort(key = lambda x : x*3, reverse = True) 
-    return str(int(''.join(numbers)))
+    numbers = list(map(str,numbers))
+    numbers.sort(key=cmp, reverse = True)
+    return str(int(''.join(numbers))) #모든 값이 0000일때는 0으로 바꿔줘야해서 int 다음 str
+
+def cmp(x):
+    for i in range(5):
+        x += x
+    return x[:5]
