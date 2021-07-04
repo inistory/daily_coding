03556d4bd611a311ed186215f1d89a50c43b4b47
Jungin <jungin3486@gmail.com>
2021-07-04 23:01:56 +0900

    210704 add solution2.py, update reame.md of 방금그곡

--- 프로그래머스/level2/방금그곡/solution2.py
+++ 프로그래머스/level2/방금그곡/solution2.py
@@ -1,19 +1,3 @@
-## 1. 문제 설명
-
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/17683)
-
-- 네오가 들은 음악의 정보가 주어질 때, 음악의 제목을 구하기
-- 각 음은 1분에 1개씩 재생
-- 조건이 일치하는 음악이 여러 개일 때에는 → 재생된 시간이 제일 긴 음악 제목을 반환
-- 재생된 시간도 같을 경우 → 먼저 입력된 음악 제목을 반환
-- 조건이 일치하는 음악이 없을 때에는 “(None)”을 반환
-- 입력
-  - m : 네오가 기억한 멜로디를 담은 문자열
-  - musicinfos : 방송된 곡의 정보를 담고 있는 배열(음악이 시작한 시각, 끝난 시각, 음악 제목, 악보 정보)
-
-## 2. 코드
-
-```python
 def replace_code(str):
     str = str.replace("C#","c")
     str = str.replace("D#","d")
@@ -22,8 +6,8 @@ def replace_code(str):
     str = str.replace("G#","g")
     str = str.replace("A#","a")
     return str
-
-
+    
+    
 def get_playtime(code):
     playtime = 0
     start_time = code[0].split(':')
@@ -38,7 +22,7 @@ def make_played_music_code(playtime,code):
         #시간(14)  - 주어진 코드길이(7) = 7 -> 코드[:7]
         q,r = divmod(playtime,len(code[3]))
         code[3] = code[3]*q + code[3][:r]
-
+        
     return code
 
 def solution(m, musicinfos):
@@ -66,9 +50,3 @@ def solution(m, musicinfos):
             if len(max_a[3]) < len(a[3]): #재생시간이 가장 긴 음악 반환
                 max_a = a
         return max_a[2]
-
-```
-
-## 3. 어려웠던 점
-
-- C#과 C를 분리하는 것
