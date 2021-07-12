676c231c49b0647582a607fa8acf86521ac00fdd
Jungin <jungin3486@gmail.com>
2021-07-12 17:56:15 +0900

    210712 update 전화번호 목록

--- 프로그래머스/level2/전화번호 목록/solution.py
+++ 프로그래머스/level2/전화번호 목록/solution.py
@@ -1,7 +1,7 @@
 def solution(phone_book):
     phone_book.sort()
     n = len(phone_book)
-    for i in range(n):
+    for i in range(1,n):
         if phone_book[i-1] in phone_book[i][:len(phone_book[i-1])]:
             return False
     return True
