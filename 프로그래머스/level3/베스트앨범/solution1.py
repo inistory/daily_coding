4a0dcd5545ec82ce4a2d817548e2b79001c313d8
Jungin <jungin3486@gmail.com>
2021-07-13 01:45:56 +0900

    210713 update solution1.py of 베스트앨범

--- 프로그래머스/level3/베스트앨범/solution1.py
+++ 프로그래머스/level3/베스트앨범/solution1.py
@@ -1,35 +1,30 @@
 def solution(genres, plays):
     n = len(genres)
-    play_count = {}
-    #장르별 재생 수
+    play_count = {} #장르별 총 재생횟수
     for i in range(n):
         if genres[i] in play_count:
             play_count[genres[i]] += plays[i]
         else:
             play_count[genres[i]] = plays[i]
-            
-        
+              
     hash_table = []
     for i in range(n):
         temp = {}
         temp['index'] = i
         temp['genres'] = genres[i]
-        temp['plays'] = plays[i]
-        temp['play_count'] = play_count[genres[i]]
+        temp['plays'] = plays[i] 
+        temp['play_count'] = play_count[genres[i]] #장르별 총 재생횟수
         hash_table.append(temp)
     
-    #속한 노래가 많이 재생된 장르를 먼저 수록
-    #장르 내에서 많이 재생된 노래를 먼저 수록합니다.
-
+    #1)속한 노래가 많이 재생된 장르를 먼저 수록, 2)장르 내에서 많이 재생된 노래를 먼저 수록
     answer = sorted(hash_table, key = lambda x : (-x['play_count'], -x['plays']))
-    check = {} #장르 두개 제한하기 위한 딕셔너리
+    check = {} #장르 갯수를 제한하기 위한 딕셔너리
     for key in list(play_count.keys()):
         check[key] = 0
         
     final = []
     for ans in answer:
-        if check[ans["genres"]] < 2:
+        if check[ans["genres"]] < 2:#장르별 갯수 두개까지로 제한
             check[ans["genres"]] += 1
-            print(check)
             final.append(ans["index"])
     return final
