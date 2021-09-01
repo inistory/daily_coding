f9d5b210e464af6f2a4307f8630d36ab7776a531
inistory <jungin3486@gmail.com>
2021-09-01 18:29:31 +0900

    210901 update 순위검색

--- 프로그래머스/level2/순위검색/solution1.py
+++ 프로그래머스/level2/순위검색/solution1.py
@@ -1,14 +1,3 @@
-## 1. 문제 설명
-
-[문제 링크](https://programmers.co.kr/learn/courses/30/lessons/72412)
-
-- Info : 지원자가 지원서에 입력한 4가지의 정보와 획득한 코딩테스트 점수를 하나의 문자열로 구성한 값의 배열
-- query : 개발팀이 궁금해하는 문의조건이 문자열 형태로 담긴 배열
-- return : 각 문의조건에 해당하는 사람들의 숫자를 순서대로 담은 배열
-
-## 2. 코드
-
-```python
 #https://hongcoding.tistory.com/56
 from itertools import combinations
 from bisect import bisect_left
@@ -43,7 +32,7 @@ def solution(info, query):
             qu_key.remove('and')
         while '-' in qu_key:  # - 제거
             qu_key.remove('-')
-
+            
         qu_key = ''.join(qu_key)  # dict의 key처럼 문자열로 변경
 
         if qu_key in DB:  # query의 key가 DB의 key로 존재하면
@@ -52,13 +41,7 @@ def solution(info, query):
                 #가져온 점수값에서 기준 점수값을 넘는 것들의 개수를 이분탐색을 통해 구한다.
                 enter = bisect_left(scores, int(qu_val))#bisect_left: score라는 정렬된 배열이 있을 때, quval이라는 새로운 수를 추가하고 싶을 때 어떤 인덱스에 넣어야하는지 알려줌
                 answer.append(len(scores) - enter)
-
         else:
             answer.append(0)
 
     return answer
-```
-
-## 3. 어려웠던 점
-
-- 효율성 점수 : 이분탐색으로 풀기
