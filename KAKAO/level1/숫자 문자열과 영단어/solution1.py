448ece9ba18e9fa1beeacff7d2dcfc5ce49dbd75
inistory <jungin3486@gmail.com>
2021-12-26 23:28:18 +0900

    211226 add solution1.py

--- KAKAO/level1/숫자 문자열과 영단어/solution1.py
+++ KAKAO/level1/숫자 문자열과 영단어/solution1.py
@@ -1,6 +1,6 @@
 def solution(S):
     num_dic = {'zero':'0','one':'1', 'two':'2','three':'3','four':'4','five':'5','six':'6','seven':'7','eight':'8','nine':'9'}
-    value_list = list(num_dic.values())
+    value_list = num_dic.values()
     result = []
     temp = ''
     for i,s in enumerate(S):
