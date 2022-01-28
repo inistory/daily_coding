2787391e7980fc350924dfc3493881bdebf44cb0
inistory <jungin3486@gmail.com>
2022-01-28 20:02:00 +0900

    update(15-1.py, 5-1): 주석, 예제

--- 이것이코딩테스트다/bsearch/15-1.py
+++ 이것이코딩테스트다/bsearch/15-1.py
@@ -1,15 +1,16 @@
 from bisect import bisect_left, bisect_right
-
+import sys
+input = sys.stdin.readline
 def count_by_range(array, left_value, right_value):
-    right_index = bisect_right(array,right_value)
-    left_index = bisect_left(array, right_value)
-    return right_index - left_index
+    right_index = bisect_right(array,right_value)#현재 값의 인덱스+1
+    left_index = bisect_left(array, right_value)#현재 값의 인덱스
+    return right_index - left_index #6 -2
 
 
-n, x = map(int,input().split())
-array = list(map(int, input().split()))
+n, x = map(int,input().split()) #7,2
+array = list(map(int, input().split()))#1 1 2 2 2 2 3
 
-count = count_by_range(array, x,x)   
+count = count_by_range(array, x,x)  
 
 if count ==0: 
     print(-1)
