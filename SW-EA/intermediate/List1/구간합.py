aa9210b4386724ffe34e106545dae361a41d6606
inistory <jungin3486@gmail.com>
2023-01-29 15:36:58 +0900

    숫자카드

---
+++ SW-EA/intermediate/List1/구간합.py
@@ -0,0 +1,9 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    #N개의 
+    N, M = map(int, input().split())
+    lst = list(map(int,input().split()))
+
+    print(N, M)
+    print(lst)
+    # print(f"{test_case} {}")
--- SW-EA/intermediate/List1/숫자카드.py
+++ SW-EA/intermediate/List1/숫자카드.py
@@ -1,4 +1,4 @@
-#test10에서 fail
+#test10에서 fail ->  카드 장수가 같을 때는 적힌 숫자가 큰 쪽을 출력 조건 추가
 T = int(input())
 for test_case in range(1, T + 1):
     tmp = {}
@@ -9,7 +9,10 @@ for test_case in range(1, T + 1):
             tmp[n] = 1
         else: 
             tmp[n] +=1
-
+    
     key = max(tmp,key=tmp.get)
-    value = tmp[key]
-    print(f"#{test_case} {key} {value}")
+    max_value = tmp[key]
+    for key, value in sorted(tmp.items(),reverse=True):#오름차순 정렬
+        if max_value == value:
+            print(f"#{test_case} {key} {value}")
+            break
