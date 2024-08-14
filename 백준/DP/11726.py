d548f03ad0e0f9d31dd5bdd72c2fb5bc8b0e4bd3
inistory <jungin3486@gmail.com>
2024-08-15 01:26:08 +0900

    [FEAT] 2xn 타일링

---
+++ 백준/DP/11726.py
@@ -0,0 +1,17 @@
+#2xn 타일링
+import sys
+input =  sys.stdin.readline
+n = int(input())
+
+dp = [0]*(n+1)
+
+if n >= 1:
+    dp[1] = 1
+if n >= 2:
+    dp[2] = 2
+if n > 2:
+    for i in range(3,n+1):
+        dp[i] = dp[i-1]+dp[i-2]
+        
+print(dp[n]%10007)
+    
