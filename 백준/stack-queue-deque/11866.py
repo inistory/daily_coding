9f0738bc11129612b1aedea8ab79de7dcfce65ff
inistory <jungin3486@gmail.com>
2025-08-05 00:51:28 +0900

    요세푸스 문제0

--- 백준/stack-queue-deque/11866.py
+++ 백준/stack-queue-deque/11866.py
@@ -5,7 +5,7 @@ queue = deque(range(1,N+1))
 result = []
 
 while queue:
-    queue.rotate(-(K - 1))     # K-1만큼 왼쪽으로 회전
-    result.append(queue.popleft())  # K번째 사람 제거
+    queue.rotate(-(K - 1))     # K-1만큼 왼쪽으로 회전(K번째 사람이 맨 앞으로 오게 만듦)
+    result.append(queue.popleft())  # 멘 앞에 있는 K번째 사람 제거
 
 print("<" + ", ".join(map(str, result)) + ">")
