8d3bf00bd92ce27df7297be382758809e650c7ed
inistory <jungin3486@gmail.com>
2025-12-21 04:11:22 +0900

    [CHORE] 문제, 접근법

--- SQL/물고기 종류 별 잡은 수 구하기.sql
+++ SQL/물고기 종류 별 잡은 수 구하기.sql
@@ -1,6 +1,8 @@
-/*물고기의 종류별 물고기의 이름과 잡은 수
+/*
+- 문제:물고기의 종류별 물고기의 이름과 잡은 수
 FISH_INFO: 잡은 물고기의 정보
 FISH_NAME_INFO: 물고기 이름에 대한 정보
+- 접근법: 조인해서 FISH_NAME별 COUNT하기
 */
 SELECT          COUNT(*) AS FISH_COUNT
                 ,FISH_NAME
