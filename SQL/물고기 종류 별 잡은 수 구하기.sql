cd2714087c159843dafc60797131568c9ac64ba2
inistory <jungin3486@gmail.com>
2025-12-21 04:13:56 +0900

    [CHORE] 테이블 주석 추가

--- SQL/물고기 종류 별 잡은 수 구하기.sql
+++ SQL/물고기 종류 별 잡은 수 구하기.sql
@@ -1,13 +1,11 @@
 /*
 - 문제:물고기의 종류별 물고기의 이름과 잡은 수
-FISH_INFO: 잡은 물고기의 정보
-FISH_NAME_INFO: 물고기 이름에 대한 정보
 - 접근법: 조인해서 FISH_NAME별 COUNT하기
 */
 SELECT          COUNT(*) AS FISH_COUNT
                 ,FISH_NAME
-FROM            FISH_INFO AS FI
-INNER JOIN      FISH_NAME_INFO AS FNI
+FROM            FISH_INFO AS FI /*잡은 물고기*/
+INNER JOIN      FISH_NAME_INFO AS FNI /*물고기 이름*/
 ON              FI.FISH_TYPE = FNI.FISH_TYPE
 GROUP BY        FISH_NAME
 ORDER BY        FISH_COUNT DESC
