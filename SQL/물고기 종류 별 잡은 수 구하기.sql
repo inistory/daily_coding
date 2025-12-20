9372cf2da61e773e7ab4cc6c6a6ddf8ad8cd34d2
inistory <jungin3486@gmail.com>
2025-12-21 04:08:21 +0900

    물고기 종류 별 잡은 수 구하기

---
+++ SQL/물고기 종류 별 잡은 수 구하기.sql
@@ -0,0 +1,11 @@
+/*물고기의 종류별 물고기의 이름과 잡은 수
+FISH_INFO: 잡은 물고기의 정보
+FISH_NAME_INFO: 물고기 이름에 대한 정보
+*/
+SELECT          COUNT(*) AS FISH_COUNT
+                ,FISH_NAME
+FROM            FISH_INFO AS FI
+INNER JOIN      FISH_NAME_INFO AS FNI
+ON              FI.FISH_TYPE = FNI.FISH_TYPE
+GROUP BY        FISH_NAME
+ORDER BY        FISH_COUNT DESC
