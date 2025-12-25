0b3a7eb6ef41d07d2561da4fc732779f6e4cb6b0
inistory <jungin3486@gmail.com>
2025-12-25 18:44:47 +0900

    [FEAT] 업그레이드 할 수 없는 아이템 구하기

---
+++ SQL/업그레이드 할 수 없는 아이템 구하기.sql
@@ -0,0 +1,14 @@
+/*
+- 문제: 더이상 업그레이드 할 수 없는 아이템 
+- 접근법: parent item이 없는 아이템만 추리기 
+*/
+SELECT          ITEM_ID
+                , ITEM_NAME
+                , RARITY 
+FROM            ITEM_INFO                         
+WHERE           ITEM_ID NOT IN (
+                SELECT          PARENT_ITEM_ID
+                FROM            ITEM_TREE
+                WHERE           PARENT_ITEM_ID IS NOT NULL -- NOT IN에서는 NULL이 있으면 안됨
+                )
+ORDER BY        ITEM_ID DESC;
