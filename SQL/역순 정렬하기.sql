b11d94b304e60bd882d421480f89ba049a816ffa
inistory <jungin3486@gmail.com>
2026-01-01 01:53:56 +0900

    [FEAT] 2026-01-01(3)

--- SQL/역순 정렬하기.sql
+++ SQL/역순 정렬하기.sql
@@ -1,8 +1,9 @@
 /*
-- 2026-01-01(2)
+- 2026-01-01(3)
 - 문제 : 역순 정렬하기
-- 접근법: DATETIME DESC로 역순 정렬
+- 접근법: ANIMAL_ID DESC로 역순 정렬
 */
-SELECT NAME, DATETIME
-FROM ANIMAL_INS
-ORDER BY ANIMAL_ID DESC
+SELECT          NAME
+                , DATETIME
+FROM            ANIMAL_INS
+ORDER BY        ANIMAL_ID DESC
