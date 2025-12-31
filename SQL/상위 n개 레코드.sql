2d86afc38a9c4b7e59c15628e804e8b1973856f6
inistory <jungin3486@gmail.com>
2025-12-31 20:45:07 +0900

    [FEAT] 2025-12-31(2)

--- SQL/상위 n개 레코드.sql
+++ SQL/상위 n개 레코드.sql
@@ -3,7 +3,7 @@
 - 문제 : 상위 n개 레코드 조회
 - 접근법: order by, limit 1 사용
 */
-SELECT NAME
-FROM ANIMAL_INS
-ORDER BY DATETIME 
-LIMIT 1
+SELECT          NAME
+FROM            ANIMAL_INS
+ORDER BY        DATETIME
+LIMIT           1
