e97fdf0fb03b7a0ac7ebbaf4276e7c9fef2f0d12
inistory <jungin3486@gmail.com>
2026-01-08 00:05:29 +0900

    [FEAT] 2026-01-07(1)

--- SQL/오랜 기간 보호한 동물(1).sql
+++ SQL/오랜 기간 보호한 동물(1).sql
@@ -3,13 +3,14 @@
 - 문제 : 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있던 동물 3마리의 이름, 보호 시작일 조회
 - 접근법: 1) ANIMAL_INS에 있는 동물 중, ANIMAL_OUTS에 없는 동물(입양못간동물) 추리기
          2) 그 중에서 가장 오래 보호소에 있던 동물 3마리 정보(ORDER BY DATETIME LIMIT 3)
+         3) ANIMAL_INS에 LEGT JOIN했을 때 ANIMAL_OUTS.ANIMAL_ID가 NULL이면 입양 못간 동물
+            (ANIMAL_INS에는 있지만 ANIMAL_OUTS에는 없는 동물이므로)
 */
-SELECT          NAME
-                , DATETIME
-FROM            ANIMAL_INS
-WHERE           ANIMAL_ID NOT IN (
-                    SELECT          ANIMAL_ID
-                    FROM            ANIMAL_OUTS
-                )
-ORDER BY        DATETIME
+SELECT          I.NAME
+                , I.DATETIME
+FROM            ANIMAL_INS I
+LEFT JOIN       ANIMAL_OUTS O
+ON              I.ANIMAL_ID = O.ANIMAL_ID
+WHERE           O.ANIMAL_ID IS NULL --입양못간동물 제외
+ORDER BY        I.DATETIME
 LIMIT           3
