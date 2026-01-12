da1d08a087e0cc06f5b374194d5fa13e80747ee3
inistory <jungin3486@gmail.com>
2026-01-12 22:23:53 +0900

    [FEAT] 2026-01-12(2)

--- SQL/Type of Triangle.sql
+++ SQL/Type of Triangle.sql
@@ -7,8 +7,8 @@
     - 두 변이 같으면 이등변 삼각형
 */
 SELECT          CASE
-                    WHEN A = B AND B = C THEN 'Equilateral'
                     WHEN A + B <= C OR A + C <= B OR B + C <= A THEN 'Not A Triangle'
+                    WHEN A = B AND B = C THEN 'Equilateral'
                     WHEN A = B OR B = C OR A = C THEN 'Isosceles'
                     WHEN A != B AND B != C AND A != C THEN 'Scalene'
                 END
