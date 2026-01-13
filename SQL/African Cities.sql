ac59e08db4719d1145454e58c81d307abe172ae5
inistory <jungin3486@gmail.com>
2026-01-13 19:40:44 +0900

    [FEAT] 2026-01-11(1)

---
+++ SQL/African Cities.sql
@@ -0,0 +1,12 @@
+/*
+- 2026-01-13(1)
+- 문제 : continent가 Africa인 city이름 구하기
+- 접근법:
+    - city와 country를 city.countrycode = country.code로 JOIN
+    - WHERE절로 continent가 Africa인 것을 필터링
+*/
+SELECT          city.name
+FROM            city
+INNER JOIN      country
+ON              city.countrycode = country.code
+WHERE           country.continent = 'Africa'
