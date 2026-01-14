21608a410077ec66be2e38f74f8394b2414f743e
inistory <jungin3486@gmail.com>
2026-01-14 20:19:48 +0900

    [FEAT] 2026-01-14(2)

--- SQL/184. Department Highest Salary.sql
+++ SQL/184. Department Highest Salary.sql
@@ -1,9 +1,11 @@
 /*
 - 2026-01-14(2)
-- 문제 : 부서별로 highest salary를 가진 employee 출력
+- 문제 : 부서별로 highest salary를 가진 employee를 개별출력
+        , 부서별 출력이아니라, employee별 출력임, 부서별로 출력하면 여러 employee를 출력할 수 없음
 - 접근법:
     - WITH문으로 부서별로 얼마를 받아야 가장 많이 받은건지 확인
     - 부서별 최대치에 해당하는 부서, 직원, 급여 출력
+    -
 */
 
 WITH
