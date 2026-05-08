8c5a9e025717382b36762313c904877f2d6c64de
inistory <jungin3486@gmail.com>
2026-05-08 23:09:16 +0900

    [FEAT] 2026-05-08(1)

--- SQL/조건에 맞는 도서 리스트 출력하기.sql
+++ SQL/조건에 맞는 도서 리스트 출력하기.sql
@@ -1,11 +1,12 @@
 /*
-- 2026-01-04(2)
+- 2026-01-04(2), 2026-05-08(1)
 - 문제 : 조건에 맞는 도서 리스트 출력하기
 - 접근법: DATE_FORMAT로 원하는 형태로 출력
 */
 SELECT          BOOK_ID
-                , DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') AS PUBLISHED_DATE
+                , DATE_FORMAT(PUBLISHED_DATE, "%Y-%m-%d") AS PUBLISHED_DATE
 FROM            BOOK
 WHERE           YEAR(PUBLISHED_DATE) = '2021'
-                AND CATEGORY='인문'
-ORDER BY        PUBLISHED_DATE
+                AND CATEGORY = '인문'
+ORDER BY        BOOK_ID
+                , PUBLISHED_DATE
